af8252641ecdfa3f27e62868b8ced6a1c1ee6b88
Emuolanari <emuolao@gmail.com>
2020-10-01 16:15:52 +0100

    type speed

--- js/hand.js
+++ js/hand.js
@@ -2,7 +2,7 @@ const landingPage = function(){
     function typeAnimation() {
         Typed.new("#writing-text", {
             strings: ["we have been expecting you.Let us deliver fresh fruits to you."],
-            typeSpeed: 9,
+            typeSpeed: 1,
             loop: true
         });
     }
