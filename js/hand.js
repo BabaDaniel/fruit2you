4670194a428ed021f53d507eece77e014ecded15
Emuolanari <emuolao@gmail.com>
2020-10-01 19:37:11 +0100

    Sign up to firebase working

--- js/hand.js
+++ js/hand.js
@@ -53,6 +53,16 @@ function validateRegistration(){
         return false;
     }
     
+    else{
+        firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // ...
+});
+
+    }
+    
 }
 
 
