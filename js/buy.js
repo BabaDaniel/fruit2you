2a585eff2fa0180f273395663eb64fae54a1fe87
Emuolanari <emuolao@gmail.com>
2020-11-19 08:48:32 +0100

    add to cart

--- js/buy.js
+++ js/buy.js
@@ -7,7 +7,7 @@ var db = firebase.firestore();
                         var description = doc.data().description;
                         var amount = doc.data().amount;
                         var url = doc.data().imageURL;
-                        document.getElementById("additems").innerHTML+= "<div class='col-lg-4 col-md-6 mb-4'><div class='card h-100'><a href='#'><img class='card-img-top' src='"+url+"' alt='"+name+"'></a><div class='card-body'><h4 class='card-title'><a href='#' style='color:  #184d47;'>"+name+"</a></h4><h5>₦"+amount+"</h5><p class='card-text'>"+description+"</p><form><script src='https://checkout.flutterwave.com/v3.js'></script><button onClick='makePayment()'>Pay Now</button></form></div><div class='card-footer'><small class='text-muted'>&#9733; &#9733; &#9733; &#9733; &#9734;</small></div></div></div>";
+                        document.getElementById("additems").innerHTML+= "<div class='col-lg-4 col-md-6 mb-4'><div class='card h-100'><a href='#'><img class='card-img-top' src='"+url+"' alt='"+name+"'></a><div class='card-body'><h4 class='card-title'><a href='#' style='color:  #184d47;'>"+name+"</a></h4><h5>₦"+amount+"</h5><p class='card-text'>"+description+"</p><form><script src='https://checkout.flutterwave.com/v3.js'></script><button type='button' onClick = 'return addToCart()'>Add To Cart</button></form></div><div class='card-footer'><small class='text-muted'>&#9733; &#9733; &#9733; &#9733; &#9734;</small></div></div></div>";
                         //console.log(doc.id, " => ", doc.data());  
                     });
                 });
